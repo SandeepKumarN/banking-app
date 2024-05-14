7fe1158fe894702a3d65f9f58b0bcd5e52d2c784
sandeep kumar n <sandeep063622@gmail.com>
2024-05-15 00:09:54 +0530

    Initial commit

---
+++ src/main/java/net/javaguides/banking/controller/AccountController.java
@@ -0,0 +1,25 @@
+package net.javaguides.banking.controller;
+
+import net.javaguides.banking.dto.AccountDto;
+import net.javaguides.banking.service.AccountService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/accounts")
+public class AccountController {
+
+    private AccountService accountService;
+
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    // Add Account REST API
+    @PostMapping
+    @RequestMapping
+    public ResponseEntity<AccountDto> addAccount(@RequestBody AccountDto accountDto) {
+        return new ResponseEntity<>(accountService.createAccount(accountDto), HttpStatus.CREATED);
+    }
+}
